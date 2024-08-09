8d31f4cfd7740d1bd5f48151dd177a5609ea0eb6
Alcadeius <andresentanu3@gmail.com>
2024-08-09 21:01:25 +0800

    Add footer and another section
    Add more section and footer

--- Project/src/App.vue
+++ Project/src/App.vue
@@ -1188,9 +1188,20 @@
         </p>
         <button
           type="button"
-          class="text-white uppercase mt-10 w-full md:w-3/4 lg:w-1/2 bg-gray-800 hover:bg-gray-900 rounded-2xl text-sm px-5 py-2.5 mb-2"
+          class="text-white inline-flex justify-center uppercase mt-10 w-full md:w-3/4 lg:w-[55%] bg-gray-800 hover:bg-gray-900 rounded-2xl text-sm px-5 py-2.5 mb-2"
         >
-          Find Services you need
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            width="30"
+            height="30"
+            viewBox="0 0 50 50"
+          >
+            <path
+              d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+            ></path>
+          </svg>
+          <span class="mt-1">Find Services you need </span>
         </button>
       </div>
       <div class="grid grid-cols-2 mt-5">
@@ -1203,6 +1214,262 @@
       </div>
     </div>
   </section>
+  <section class="mx-16 mt-20">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <div class="flex flex-col font-jakarta mb-5">
+        <p class="text-blue-800 mb-5 text-xs font-bold">Support</p>
+        <h1 class="text-2xl font-ubuntu">Frequently Asked Question</h1>
+        <p class="font-jakarta text-sm text-black/50 mb-5">
+          Here,you'll answers to a wide range of common inquiries,providing you
+          with the insight and clarity you need.
+        </p>
+        <div class="rounded-lg border w-full p-4">
+          <div class="flex w-1/2 max-w-20">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              class="w-14 h-14 relative left-1 top-1"
+              xml:space="preserve"
+            >
+              <path
+                d="M16 31C7.729 31 1 24.271 1 16S7.729 1 16 1s15 6.729 15 15-6.729 15-15 15zm0-28C8.832 3 3 8.832 3 16s5.832 13 13 13 13-5.832 13-13S23.168 3 16 3z"
+              />
+              <circle cx="16" cy="15.133" r="4.267" />
+              <path
+                d="M16 30c2.401 0 4.66-.606 6.635-1.671-.425-3.229-3.18-5.82-6.635-5.82s-6.21 2.591-6.635 5.82A13.935 13.935 0 0 0 16 30z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              class="w-14 h-14 relative left-1 top-1"
+              xml:space="preserve"
+            >
+              <path
+                d="M16 31C7.729 31 1 24.271 1 16S7.729 1 16 1s15 6.729 15 15-6.729 15-15 15zm0-28C8.832 3 3 8.832 3 16s5.832 13 13 13 13-5.832 13-13S23.168 3 16 3z"
+              />
+              <circle cx="16" cy="15.133" r="4.267" />
+              <path
+                d="M16 30c2.401 0 4.66-.606 6.635-1.671-.425-3.229-3.18-5.82-6.635-5.82s-6.21 2.591-6.635 5.82A13.935 13.935 0 0 0 16 30z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              class="w-14 h-14 relative left-1 top-1"
+              xml:space="preserve"
+            >
+              <path
+                d="M16 31C7.729 31 1 24.271 1 16S7.729 1 16 1s15 6.729 15 15-6.729 15-15 15zm0-28C8.832 3 3 8.832 3 16s5.832 13 13 13 13-5.832 13-13S23.168 3 16 3z"
+              />
+              <circle cx="16" cy="15.133" r="4.267" />
+              <path
+                d="M16 30c2.401 0 4.66-.606 6.635-1.671-.425-3.229-3.18-5.82-6.635-5.82s-6.21 2.591-6.635 5.82A13.935 13.935 0 0 0 16 30z"
+              />
+            </svg>
+          </div>
+          <p class="font-jakarta mt-3 font-semibold mb-3">
+            Still Have Question
+          </p>
+          <p class="font-jakarta text-sm mb-3">
+            Can't Find the answer you're looking for? please chat our team
+          </p>
+          <button
+            type="button"
+            class="text-white bg-gray-800 hover:bg-gray-900 rounded-xl text-sm px-5 py-2.5 me-2 mb-2"
+          >
+            Get in touch
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-col font-jakarta">
+        <div class="p-10 rounded-xl bg-black/5">
+          <div class="flex-row flex justify-between mb-10">
+            <h1 class="font-bold">Who needs the B211A visa?</h1>
+            <span
+              ><svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                class="w-6 h-6"
+              >
+                <path
+                  d="M8 1a7 7 0 1 1-7 7 7 7 0 0 1 7-7m0-1a8 8 0 1 0 8 8 8 8 0 0 0-8-8z"
+                />
+                <path d="M4 7h8v2H4z" />
+              </svg>
+            </span>
+          </div>
+          <ul class="text-black/65">
+            <li class="mb-3">
+              1.You want to stay in Indonesia for more than 60 days
+            </li>
+            <li class="mb-3">
+              2.You don't know how long you want to stay and you want to keep
+              the option open for longer than 60 days without having to leave
+              the country
+            </li>
+            <li class="mb-3">
+              3.After the visa you want to you apply for
+              KITAS(investor,marriage,pension)
+            </li>
+            <li class="mb-3">
+              4.your reason for entering the country is not for tourism
+              purposes(e.g. volunteer work,internship,further education etc.)
+            </li>
+            <li class="mb-3">
+              5.You stay longer than 30 days and travel a lot and can Not coming
+              to the Immigration Office for the extension(personal appearance
+              important).
+            </li>
+            <li class="mb-3">
+              6.You only have a temporary passport and want to enter indonesia
+            </li>
+            <li>
+              7.It is uncertain whether the Visa On Arrival will be granted due
+              to your vaccination status or other formalities and you want to be
+              on the safe side.
+            </li>
+          </ul>
+        </div>
+        <div
+          class="flex-row flex justify-between mb-10 mx-10 mt-10 cursor-pointer"
+        >
+          <h1 class="font-bold">How long is the visa valid?</h1>
+          <span>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              class="w-6 h-6"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8zm0 15a7 7 0 1 1 7-7 7 7 0 0 1-7 7z"
+              />
+              <path d="M9 4H7v3H4v2h3v3h2V9h3V7H9V4z" />
+            </svg>
+          </span>
+        </div>
+        <div class="flex-row flex justify-between mb-10 mx-10 cursor-pointer">
+          <h1 class="font-bold">
+            How long does it take for my visa to be ready?
+          </h1>
+          <span>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              class="w-6 h-6"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8zm0 15a7 7 0 1 1 7-7 7 7 0 0 1-7 7z"
+              />
+              <path d="M9 4H7v3H4v2h3v3h2V9h3V7H9V4z" />
+            </svg>
+          </span>
+        </div>
+        <div class="flex-row flex justify-between mb-10 mx-10 cursor-pointer">
+          <h1 class="font-bold">
+            How often does it happen that a visa application is rejected?
+          </h1>
+          <span>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              class="w-6 h-6"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8zm0 15a7 7 0 1 1 7-7 7 7 0 0 1-7 7z"
+              />
+              <path d="M9 4H7v3H4v2h3v3h2V9h3V7H9V4z" />
+            </svg>
+          </span>
+        </div>
+        <div class="flex-row flex justify-between mb-10 mx-10 cursor-pointer">
+          <h1 class="font-bold">Who is my sponsor?</h1>
+          <span>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              class="w-6 h-6"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8zm0 15a7 7 0 1 1 7-7 7 7 0 0 1-7 7z"
+              />
+              <path d="M9 4H7v3H4v2h3v3h2V9h3V7H9V4z" />
+            </svg>
+          </span>
+        </div>
+        <div class="flex-row flex justify-between mb-10 mx-10 cursor-pointer">
+          <h1 class="font-bold">Do i have to go into quarantine upon entry?</h1>
+          <span>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              class="w-6 h-6"
+            >
+              <path
+                d="M8 0a8 8 0 1 0 8 8 8 8 0 0 0-8-8zm0 15a7 7 0 1 1 7-7 7 7 0 0 1-7 7z"
+              />
+              <path d="M9 4H7v3H4v2h3v3h2V9h3V7H9V4z" />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="bg-black p-20 overflow-hidden relative">
+    <div class="font-jakarta text-white text-center mx-auto relative top-60">
+      <h1 class="font-ubuntu text-3xl">
+        Apply for your visa today, quick and easy
+      </h1>
+      <p class="text-white/35 text-lg relative top-5 z-10">
+        it's all online to create it! so no worries.Its quick and easy
+      </p>
+      <button
+        class="bg-white p-3 rounded-full relative w-1/2 md:w-[30%] lg:w-[20%] top-16 z-10"
+      >
+        <span
+          class="uppercase text-black font-jakarta font-bold flex justify-center content-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-7 h-7 mr-1"
+            viewBox="0 0 50 50"
+          >
+            <path
+              d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+            ></path>
+          </svg>
+          Find services
+        </span>
+      </button>
+      <button class="bg-[#09406C] p-4 rounded-full relative top-14 left-3 z-10">
+        <span class="uppercase text-white font-jakarta font-semibold">
+          Consult now
+        </span>
+      </button>
+      <div class="font-jakarta relative top-52 z-10">
+        <p>Copyright &copy; Visa Indonesia 2023</p>
+        <p class="text-white/35">Design & Developed by s.p Digital</p>
+      </div>
+    </div>
+    <p
+      class="w-96 h-96 relative opacity-50 -bottom-60 left-[35%] mt-20 bg-gradient-to-l from-blue-800 to-purple-950 rounded-full blur-3xl"
+    ></p>
+    <p
+      class="border absolute border-white/20 w-[35rem] h-[35rem] left-[30%] -bottom-80 rounded-full"
+    ></p>
+    <p
+      class="border absolute border-white/20 w-[60rem] h-[60rem] left-[15%] -bottom-[35rem] rounded-full"
+    ></p>
+    <p
+      class="border absolute border-white/20 w-[75rem] h-[75rem] left-[5%] -bottom-[40rem] rounded-full"
+    ></p>
+  </footer>
 </template>
 
 <script>
